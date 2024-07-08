a5ce64ce8528557153a491336c76f47d917c3f82
นิอามีน นิอามัน <niamin.niaman@gmail.com>
2024-07-08 07:24:54 +0000

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { auth } from '@/auth';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Next Shadcn',
-  description: 'Basic dashboard with Next.js and Shadcn'
+  title: 'Shadcn Template',
+  description: 'Shadcn Template'
 };
 
 export default async function RootLayout({
